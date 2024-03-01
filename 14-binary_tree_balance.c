8c246f9fa48d9475f27d6cea6b7ab62acc005892
goldenjeffempire <jeffemuodafe124@gmail.com>
2024-03-01 15:40:35 -0800

    Binary

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,10 +9,10 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-        if (tree)
-                return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
-        return (0);
+	return (0);
 }
 
 /**
@@ -24,15 +24,15 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        if (tree)
-        {
-                size_t lef = 0, rig = 0;
+	if (tree)
+	{
+		size_t lef = 0, rig = 0;
 
-                lef = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-                rig = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		lef = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		rig = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 
-                return ((lef > rig) ? lef : rig);
-        }
+		return ((lef > rig) ? lef : rig);
+	}
 
-        return (0);
+	return (0);
 }
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,7 +2,7 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree);
 int is_perfect_recursive(const binary_tree_t *tree,
-                size_t leaf_depth, size_t level);
+		size_t leaf_depth, size_t level);
 const binary_tree_t *get_leaf(const binary_tree_t *tree);
 size_t depth(const binary_tree_t *tree);
 unsigned char is_leaf(const binary_tree_t *node);
@@ -16,9 +16,9 @@ unsigned char is_leaf(const binary_tree_t *node);
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-        if (tree == NULL)
-                return (0);
-        return (is_perfect_recursive(tree, depth(get_leaf(tree)), 0));
+	if (tree == NULL)
+		return (0);
+	return (is_perfect_recursive(tree, depth(get_leaf(tree)), 0));
 }
 
 /**
@@ -30,7 +30,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
  */
 size_t depth(const binary_tree_t *tree)
 {
-        return (tree->parent != NULL ? 1 + depth(tree->parent) : 0);
+	return (tree->parent != NULL ? 1 + depth(tree->parent) : 0);
 }
 
 /**
@@ -41,9 +41,9 @@ size_t depth(const binary_tree_t *tree)
  */
 const binary_tree_t *get_leaf(const binary_tree_t *tree)
 {
-        if (is_leaf(tree) == 1)
-                return (tree);
-        return (tree->left ? get_leaf(tree->left) : get_leaf(tree->right));
+	if (is_leaf(tree) == 1)
+		return (tree);
+	return (tree->left ? get_leaf(tree->left) : get_leaf(tree->right));
 }
 
 /**
@@ -55,14 +55,14 @@ const binary_tree_t *get_leaf(const binary_tree_t *tree)
  * Return: If the tree is perfect, 1, otherwise 0.
  */
 int is_perfect_recursive(const binary_tree_t *tree,
-                size_t leaf_depth, size_t level)
+		size_t leaf_depth, size_t level)
 {
-        if (is_leaf(tree))
-                return (level == leaf_depth ? 1 : 0);
-        if (tree->left == NULL || tree->right == NULL)
-                return (0);
-        return (is_perfect_recursive(tree->left, leaf_depth, level + 1) &&
-                is_perfect_recursive(tree->right, leaf_depth, level + 1));
+	if (is_leaf(tree))
+		return (level == leaf_depth ? 1 : 0);
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+	return (is_perfect_recursive(tree->left, leaf_depth, level + 1) &&
+		is_perfect_recursive(tree->right, leaf_depth, level + 1));
 }
 
 /**
@@ -73,5 +73,5 @@ int is_perfect_recursive(const binary_tree_t *tree,
  */
 unsigned char is_leaf(const binary_tree_t *node)
 {
-        return ((node->left == NULL && node->right == NULL) ? 1 : 0);
+	return ((node->left == NULL && node->right == NULL) ? 1 : 0);
 }
